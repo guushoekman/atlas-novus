9f6abd0acfa1d224edf29000b7474b3a568b766a
Guus Hoekman <mail@guushoekman.com>
2018-05-26 19:04:49 +0200

    add warning message to posters

--- js/atlasnovus.js
+++ js/atlasnovus.js
@@ -3,9 +3,13 @@ $(".card-footer.sizes .card-footer-item").click(function(e) {
   $(this).parent().prev(".card-footer.download").addClass("visible");
   $(this).siblings().removeClass("active");
   $(this).addClass("active");
-  console.log($(this).attr("size"));
   $(this).parent().prev().children().children(".size").text($(this).attr("size"));
   $(this).parent().prev().children().children(".width").text($(this).attr("width"));
   $(this).parent().prev().children().children(".height").text($(this).attr("height"));
   $(this).parent().prev().children().children("a").attr("href", ($(this).attr("url")));
+  if ( $(this).hasClass("poster") ) {
+    $(this).parent().prev().prev(".card-footer.warning").addClass("visible");
+  } else {
+    $(this).parent().prev().prev(".card-footer.warning").removeClass("visible");
+  }
 })
